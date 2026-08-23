d5c2fe18c62a68258ca170a4fc0cf640d57d3cc0
h0616-m <habibmamsa349@gmail.com>
2026-08-24 00:20:08 +0530

    added login logic

---
+++ src/main/java/com/hobby/hub/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.hobby.hub.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public boolean validateUser(String username, String password) {
+        String sql = "SELECT COUNT(*) FROM users WHERE username = ? AND password = ?";
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, username, password);
+        return count != null && count > 0;
+    }
+}
